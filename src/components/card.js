97d3590ff20e50fcadaee463fee9fd5d027ff8b7
KeithSilcock <silcockk@gmail.com>
2018-06-04 15:57:20 -0700

    started adding functionality to individual cards

--- src/components/card.js
+++ src/components/card.js
@@ -3,12 +3,14 @@ import cardBack from '../assets/images/cardBack.jpg';
 
 class Card extends React.Component {
 
+    cardClicked(){
+        console.log('Sup')
+    }
+
     render(){
 
-
-
         return(
-            <div className='card'>
+            <div className='card' onClick={this.cardClicked.bind(this)}>
                 <span>{this.props.text}</span>
                 <img src={cardBack} alt="card back" className='cardImg'/>
             </div>
